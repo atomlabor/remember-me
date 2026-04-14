5ad6f05cada8652d9d03b7c1070932fac5e24b8b
Atomlabor <46759744+atomlabor@users.noreply.github.com>
2026-04-14 09:36:43 +0200

    Add files via upload

--- src/c/main.c
+++ src/c/main.c
@@ -1,10 +1,17 @@
 #include <pebble.h>
 
-#define KEY_REMINDER_1     0
-#define KEY_REMINDER_2     1
-#define KEY_REMINDER_3     2
-#define KEY_COLOR_TOP      3
-#define KEY_OUTLINE_WHITE  4
+#define KEY_REMINDER_1       0
+#define KEY_REMINDER_2       1
+#define KEY_REMINDER_3       2
+#define KEY_COLOR_TOP        3
+#define KEY_COLOR_BOTTOM     4
+#define KEY_OUTLINE_WHITE    5
+#define KEY_COLOR_TEXT       6
+#define KEY_HIDE_ICON        7
+#define KEY_ENABLE_GRADIENT  8
+#define KEY_COLOR_BOTTOM_END 9
+#define KEY_COLOR_TIME       10
+#define KEY_COLOR_BATTERY    11
 
 static Window *s_main_window;
 static Layer *s_canvas_layer;
@@ -22,13 +29,56 @@ static char s_reminder_3[64] = "Stay Focused";
 static int s_current_index = 0;
 static int s_battery_level = 100;
 
-static GColor s_color_top;
-static bool s_outline_white;
+static GColor s_color_top, s_color_bottom, s_color_bottom_end, s_color_text, s_color_time, s_color_battery;
+static bool s_outline_white, s_hide_icon, s_enable_gradient;
+
+static int get_layout_offset() {
+    #if defined(PBL_BW) || defined(PBL_PLATFORM_BASALT)
+        return 5;
+    #else
+        return 0;
+    #endif
+}
+
+static void update_layout() {
+    Layer *window_layer = window_get_root_layer(s_main_window);
+    GRect bounds = layer_get_bounds(window_layer);
+    
+    int top_height = (bounds.size.h / 3) + PBL_IF_ROUND_ELSE(10, 0) + get_layout_offset();
+    int accent_height = 8;
+    int icon_y = top_height + 4 - 12;
+    
+    if (s_hide_icon) {
+        layer_set_hidden(bitmap_layer_get_layer(s_icon_layer), true);
+        int reminder_area_top = top_height + accent_height; 
+        int reminder_area_height = bounds.size.h - reminder_area_top;
+        layer_set_frame(text_layer_get_layer(s_reminder_layer), GRect(0, reminder_area_top, bounds.size.w, reminder_area_height));
+    } else {
+        layer_set_hidden(bitmap_layer_get_layer(s_icon_layer), false);
+        int reminder_area_top = icon_y + 29;
+        int reminder_area_height = bounds.size.h - reminder_area_top;
+        layer_set_frame(text_layer_get_layer(s_reminder_layer), GRect(0, reminder_area_top, bounds.size.w, reminder_area_height));
+    }
+
+    #if defined(PBL_COLOR)
+        text_layer_set_text_color(s_reminder_layer, s_color_text);
+    #else
+        text_layer_set_text_color(s_reminder_layer, gcolor_equal(s_color_bottom, GColorBlack) ? GColorWhite : GColorBlack);
+    #endif
+}
+
+static void set_responsive_font(const char* text) {
+    int len = strlen(text);
+    if (len < 15) text_layer_set_font(s_reminder_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+    else if (len < 25) text_layer_set_font(s_reminder_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+    else text_layer_set_font(s_reminder_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+}
 
 static void update_reminder_display() {
-    if (s_current_index == 0) text_layer_set_text(s_reminder_layer, s_reminder_1);
-    else if (s_current_index == 1) text_layer_set_text(s_reminder_layer, s_reminder_2);
-    else text_layer_set_text(s_reminder_layer, s_reminder_3);
+    const char* active_text = (s_current_index == 0) ? s_reminder_1 : (s_current_index == 1 ? s_reminder_2 : s_reminder_3);
+    set_responsive_font(active_text);
+    text_layer_set_text(s_reminder_layer, active_text);
+    update_layout();
 }
 
 static void tap_handler(AccelAxisType axis, int32_t direction) {
@@ -49,61 +99,101 @@ static void update_time() {
 }
 
 static void inbox_received_handler(DictionaryIterator *iter, void *context) {
-    Tuple *rem1_t = dict_find(iter, MESSAGE_KEY_CONFIG_REMINDER_1);
-    if (rem1_t) {
-        strncpy(s_reminder_1, rem1_t->value->cstring, sizeof(s_reminder_1)-1);
-        persist_write_string(KEY_REMINDER_1, s_reminder_1);
-    }
-    Tuple *rem2_t = dict_find(iter, MESSAGE_KEY_CONFIG_REMINDER_2);
-    if (rem2_t) {
-        strncpy(s_reminder_2, rem2_t->value->cstring, sizeof(s_reminder_2)-1);
-        persist_write_string(KEY_REMINDER_2, s_reminder_2);
-    }
-    Tuple *rem3_t = dict_find(iter, MESSAGE_KEY_CONFIG_REMINDER_3);
-    if (rem3_t) {
-        strncpy(s_reminder_3, rem3_t->value->cstring, sizeof(s_reminder_3)-1);
-        persist_write_string(KEY_REMINDER_3, s_reminder_3);
-    }
-    Tuple *col_t = dict_find(iter, MESSAGE_KEY_CONFIG_COLOR_TOP);
-    if (col_t) {
-        s_color_top = GColorFromHEX(col_t->value->uint32);
-        persist_write_int(KEY_COLOR_TOP, col_t->value->uint32);
-    }
-    Tuple *out_t = dict_find(iter, MESSAGE_KEY_CONFIG_OUTLINE_WHITE);
-    if (out_t) {
-        s_outline_white = out_t->value->int32 == 1;
-        persist_write_bool(KEY_OUTLINE_WHITE, s_outline_white);
-    }
+    Tuple *t;
+    if ((t = dict_find(iter, MESSAGE_KEY_CONFIG_REMINDER_1))) { strncpy(s_reminder_1, t->value->cstring, 63); persist_write_string(KEY_REMINDER_1, s_reminder_1); }
+    if ((t = dict_find(iter, MESSAGE_KEY_CONFIG_REMINDER_2))) { strncpy(s_reminder_2, t->value->cstring, 63); persist_write_string(KEY_REMINDER_2, s_reminder_2); }
+    if ((t = dict_find(iter, MESSAGE_KEY_CONFIG_REMINDER_3))) { strncpy(s_reminder_3, t->value->cstring, 63); persist_write_string(KEY_REMINDER_3, s_reminder_3); }
+    
+    if ((t = dict_find(iter, MESSAGE_KEY_CONFIG_COLOR_TOP))) { s_color_top = GColorFromHEX(t->value->uint32); persist_write_int(KEY_COLOR_TOP, t->value->uint32); }
+    if ((t = dict_find(iter, MESSAGE_KEY_CONFIG_COLOR_BOTTOM))) { s_color_bottom = GColorFromHEX(t->value->uint32); persist_write_int(KEY_COLOR_BOTTOM, t->value->uint32); }
+    if ((t = dict_find(iter, MESSAGE_KEY_CONFIG_COLOR_BOTTOM_END))) { s_color_bottom_end = GColorFromHEX(t->value->uint32); persist_write_int(KEY_COLOR_BOTTOM_END, t->value->uint32); }
+    if ((t = dict_find(iter, MESSAGE_KEY_CONFIG_COLOR_TEXT))) { s_color_text = GColorFromHEX(t->value->uint32); persist_write_int(KEY_COLOR_TEXT, t->value->uint32); }
+    if ((t = dict_find(iter, MESSAGE_KEY_CONFIG_COLOR_TIME))) { s_color_time = GColorFromHEX(t->value->uint32); persist_write_int(KEY_COLOR_TIME, t->value->uint32); }
+    if ((t = dict_find(iter, MESSAGE_KEY_CONFIG_COLOR_BATTERY))) { s_color_battery = GColorFromHEX(t->value->uint32); persist_write_int(KEY_COLOR_BATTERY, t->value->uint32); }
+    
+    if ((t = dict_find(iter, MESSAGE_KEY_CONFIG_OUTLINE_WHITE))) { s_outline_white = t->value->int32 == 1; persist_write_bool(KEY_OUTLINE_WHITE, s_outline_white); }
+    if ((t = dict_find(iter, MESSAGE_KEY_CONFIG_HIDE_ICON))) { s_hide_icon = t->value->int32 == 1; persist_write_bool(KEY_HIDE_ICON, s_hide_icon); }
+    if ((t = dict_find(iter, MESSAGE_KEY_CONFIG_ENABLE_GRADIENT))) { s_enable_gradient = t->value->int32 == 1; persist_write_bool(KEY_ENABLE_GRADIENT, s_enable_gradient); }
+    
     update_reminder_display();
-    if (s_canvas_layer) layer_mark_dirty(s_canvas_layer);
+    layer_mark_dirty(s_canvas_layer);
 }
 
 static void canvas_update_proc(Layer *layer, GContext *ctx) {
     GRect bounds = layer_get_bounds(layer);
-    GColor color_accent = PBL_IF_COLOR_ELSE(GColorSunsetOrange, GColorBlack);
-    GColor color_bottom = PBL_IF_COLOR_ELSE(GColorPastelYellow, GColorWhite);
-
-    int top_height = (bounds.size.h / 3) + PBL_IF_ROUND_ELSE(10, 0);
+    int top_height = (bounds.size.h / 3) + PBL_IF_ROUND_ELSE(10, 0) + get_layout_offset();
     int accent_height = 8; 
     
     graphics_context_set_fill_color(ctx, s_color_top);
     graphics_fill_rect(ctx, GRect(0, 0, bounds.size.w, top_height), 0, GCornerNone);
-    graphics_context_set_fill_color(ctx, color_accent);
+    graphics_context_set_fill_color(ctx, PBL_IF_COLOR_ELSE(GColorSunsetOrange, GColorBlack));
     graphics_fill_rect(ctx, GRect(0, top_height, bounds.size.w, accent_height), 0, GCornerNone);
-    graphics_context_set_fill_color(ctx, color_bottom);
-    graphics_fill_rect(ctx, GRect(0, top_height + accent_height, bounds.size.w, bounds.size.h - (top_height + accent_height)), 0, GCornerNone);
+    
+    // SMOOTH GRADIENT DITHERING ENGINE
+    #if defined(PBL_COLOR)
+        if (s_enable_gradient) {
+            int start_y = top_height + accent_height;
+            int end_y = bounds.size.h;
+            int h = end_y - start_y;
+            if (h > 0) {
+                // Bayer Matrix 4x4 pre-scaled für Pebble Farben
+                static const int bayer[4][4] = {
+                    { -40,   0, -30,  10 },
+                    {  20, -20,  30, -10 },
+                    { -25,  15, -35,   5 },
+                    {  35,  -5,  25, -15 }
+                };
+                for(int y = start_y; y < end_y; y++) {
+                    int step = y - start_y;
+                    // Skalierung auf 0-255 für GColor
+                    int r_interp = (s_color_bottom.r * 85) + (((s_color_bottom_end.r - s_color_bottom.r) * 85 * step) / h);
+                    int g_interp = (s_color_bottom.g * 85) + (((s_color_bottom_end.g - s_color_bottom.g) * 85 * step) / h);
+                    int b_interp = (s_color_bottom.b * 85) + (((s_color_bottom_end.b - s_color_bottom.b) * 85 * step) / h);
 
+                    for(int x = 0; x < bounds.size.w; x++) {
+                        int offset = bayer[y % 4][x % 4];
+                        int r = r_interp + offset;
+                        int g = g_interp + offset;
+                        int b = b_interp + offset;
+
+                        r = (r < 0) ? 0 : ((r > 255) ? 255 : r);
+                        g = (g < 0) ? 0 : ((g > 255) ? 255 : g);
+                        b = (b < 0) ? 0 : ((b > 255) ? 255 : b);
+
+                        graphics_context_set_stroke_color(ctx, GColorFromRGB(r, g, b));
+                        graphics_draw_pixel(ctx, GPoint(x, y));
+                    }
+                }
+            }
+        } else {
+            graphics_context_set_fill_color(ctx, s_color_bottom);
+            graphics_fill_rect(ctx, GRect(0, top_height + accent_height, bounds.size.w, bounds.size.h - (top_height + accent_height)), 0, GCornerNone);
+        }
+    #else
+        graphics_context_set_fill_color(ctx, s_color_bottom);
+        graphics_fill_rect(ctx, GRect(0, top_height + accent_height, bounds.size.w, bounds.size.h - (top_height + accent_height)), 0, GCornerNone);
+    #endif
+
+    // Battery Rendering
+    #if defined(PBL_BW)
+    graphics_context_set_compositing_mode(ctx, GCompOpAssignInverted);
+    #endif
+    graphics_context_set_stroke_color(ctx, s_color_battery);
+    graphics_context_set_fill_color(ctx, s_color_battery);
+    
     int total_bat_w = (10 * 6) + (9 * 3);
     int start_x = (bounds.size.w - total_bat_w) / 2;
     int active_blocks = (s_battery_level + 5) / 10;
-    graphics_context_set_stroke_color(ctx, PBL_IF_COLOR_ELSE(GColorRed, GColorBlack));
-    graphics_context_set_fill_color(ctx, PBL_IF_COLOR_ELSE(GColorRed, GColorBlack));
     for (int i = 0; i < 10; i++) {
         GRect r = GRect(start_x + i * 9, PBL_IF_ROUND_ELSE(14, 2), 6, 4);
         if (i < active_blocks) graphics_fill_rect(ctx, r, 1, GCornersAll); 
         else graphics_draw_round_rect(ctx, r, 1);
     }
+    #if defined(PBL_BW)
+    graphics_context_set_compositing_mode(ctx, GCompOpAssign);
+    #endif
 
+    // Time Rendering
     GRect tb = GRect(0, s_time_y, bounds.size.w, s_time_h);
     graphics_context_set_text_color(ctx, s_outline_white ? GColorWhite : GColorBlack);
     for (int dx = -2; dx <= 2; dx++) {
@@ -112,7 +202,7 @@ static void canvas_update_proc(Layer *layer, GContext *ctx) {
             graphics_draw_text(ctx, s_time_buffer, s_time_font, GRect(tb.origin.x + dx, tb.origin.y + dy, tb.size.w, tb.size.h), GTextOverflowModeTrailingEllipsis, GTextAlignmentCenter, NULL);
         }
     }
-    graphics_context_set_text_color(ctx, PBL_IF_COLOR_ELSE(GColorRed, GColorWhite));
+    graphics_context_set_text_color(ctx, s_color_time);
     graphics_draw_text(ctx, s_time_buffer, s_time_font, tb, GTextOverflowModeTrailingEllipsis, GTextAlignmentCenter, NULL);
 }
 
@@ -135,7 +225,7 @@ static void main_window_load(Window *window) {
     layer_set_update_proc(s_canvas_layer, canvas_update_proc);
     layer_add_child(window_layer, s_canvas_layer);
 
-    int top_height = (bounds.size.h / 3) + PBL_IF_ROUND_ELSE(10, 0);
+    int top_height = (bounds.size.h / 3) + PBL_IF_ROUND_ELSE(10, 0) + get_layout_offset();
     s_reminder_icon = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_WATCH_ICON);
     int icon_y = top_height + 4 - 12; 
     s_icon_layer = bitmap_layer_create(GRect((bounds.size.w - 25) / 2, icon_y, 25, 25));
@@ -143,19 +233,17 @@ static void main_window_load(Window *window) {
     bitmap_layer_set_compositing_mode(s_icon_layer, GCompOpSet); 
     layer_add_child(window_layer, bitmap_layer_get_layer(s_icon_layer));
 
-    int margin_x = PBL_IF_ROUND_ELSE(bounds.size.w / 6, 10);
-    s_reminder_layer = text_layer_create(GRect(margin_x, icon_y + 31, bounds.size.w - (2 * margin_x), bounds.size.h - (icon_y + 31)));
+    s_reminder_layer = text_layer_create(GRect(0, 0, bounds.size.w, bounds.size.h));
     text_layer_set_background_color(s_reminder_layer, GColorClear);
-    text_layer_set_text_color(s_reminder_layer, PBL_IF_COLOR_ELSE(GColorOxfordBlue, GColorBlack));
-    text_layer_set_font(s_reminder_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
     text_layer_set_text_alignment(s_reminder_layer, GTextAlignmentCenter);
-    
-    if (persist_exists(KEY_REMINDER_1)) persist_read_string(KEY_REMINDER_1, s_reminder_1, sizeof(s_reminder_1));
-    if (persist_exists(KEY_REMINDER_2)) persist_read_string(KEY_REMINDER_2, s_reminder_2, sizeof(s_reminder_2));
-    if (persist_exists(KEY_REMINDER_3)) persist_read_string(KEY_REMINDER_3, s_reminder_3, sizeof(s_reminder_3));
-    
-    update_reminder_display();
     layer_add_child(window_layer, text_layer_get_layer(s_reminder_layer));
+    
+    if (persist_exists(KEY_REMINDER_1)) persist_read_string(KEY_REMINDER_1, s_reminder_1, 64);
+    if (persist_exists(KEY_REMINDER_2)) persist_read_string(KEY_REMINDER_2, s_reminder_2, 64);
+    if (persist_exists(KEY_REMINDER_3)) persist_read_string(KEY_REMINDER_3, s_reminder_3, 64);
+    
+    update_layout();
+    update_reminder_display();
 }
 
 static void main_window_unload(Window *window) {
@@ -166,7 +254,16 @@ static void main_window_unload(Window *window) {
 
 static void init() {
     s_color_top = persist_exists(KEY_COLOR_TOP) ? GColorFromHEX(persist_read_int(KEY_COLOR_TOP)) : GColorPictonBlue;
+    s_color_bottom = persist_exists(KEY_COLOR_BOTTOM) ? GColorFromHEX(persist_read_int(KEY_COLOR_BOTTOM)) : PBL_IF_COLOR_ELSE(GColorPastelYellow, GColorWhite);
+    s_color_bottom_end = persist_exists(KEY_COLOR_BOTTOM_END) ? GColorFromHEX(persist_read_int(KEY_COLOR_BOTTOM_END)) : GColorPictonBlue;
+    s_color_text = persist_exists(KEY_COLOR_TEXT) ? GColorFromHEX(persist_read_int(KEY_COLOR_TEXT)) : GColorOxfordBlue;
+    s_color_time = persist_exists(KEY_COLOR_TIME) ? GColorFromHEX(persist_read_int(KEY_COLOR_TIME)) : PBL_IF_COLOR_ELSE(GColorRed, GColorWhite);
+    s_color_battery = persist_exists(KEY_COLOR_BATTERY) ? GColorFromHEX(persist_read_int(KEY_COLOR_BATTERY)) : PBL_IF_COLOR_ELSE(GColorRed, GColorBlack);
+    
     s_outline_white = persist_exists(KEY_OUTLINE_WHITE) ? persist_read_bool(KEY_OUTLINE_WHITE) : false;
+    s_hide_icon = persist_exists(KEY_HIDE_ICON) ? persist_read_bool(KEY_HIDE_ICON) : false;
+    s_enable_gradient = persist_exists(KEY_ENABLE_GRADIENT) ? persist_read_bool(KEY_ENABLE_GRADIENT) : false;
+    
     s_main_window = window_create();
     window_set_window_handlers(s_main_window, (WindowHandlers) {.load = main_window_load, .unload = main_window_unload});
     window_stack_push(s_main_window, true);
--- src/pkjs/config.js
+++ src/pkjs/config.js
@@ -1,79 +1,53 @@
 module.exports = [
   {
     "type": "heading",
-    "defaultValue": "Remember Me"
+    "defaultValue": "Remind Me"
   },
   {
     "type": "text",
-    "defaultValue": "Welcome to your personal 'Keep in Mind' watchface. This app is designed to help you stay focused on your goals, habits, or positive thoughts. By displaying your custom reminders directly under the time, you'll subconsciously process them every time you check the watch."
+    "defaultValue": "Welcome to your personal 'Keep in Mind' watchface. This app is designed to help you stay focused on your goals, habits, or positive thoughts. By displaying your custom reminders directly under the time, you subconsciously process them every time you check the watch."
   },
   {
     "type": "section",
     "items": [
-      {
-        "type": "heading",
-        "defaultValue": "Tutorial"
-      },
-      {
-        "type": "text",
-        "defaultValue": "Enter up to three different reminders below. You can cycle through them on your watch by either **shaking your wrist** or **tapping the screen**."
-      }
+      { "type": "heading", "defaultValue": "Tutorial" },
+      { "type": "text", "defaultValue": "Enter up to three different reminders below. You can cycle through them on your watch by either **shaking your wrist** or **tapping the screen**. The font size will automatically adjust to fit your text perfectly." }
     ]
   },
   {
     "type": "section",
     "items": [
-      {
-        "type": "heading",
-        "defaultValue": "Your Reminders"
-      },
-      {
-        "type": "input",
-        "messageKey": "CONFIG_REMINDER_1",
-        "defaultValue": "Keep Smiling",
-        "label": "First Reminder",
-        "attributes": { "maxlength": 64 }
-      },
-      {
-        "type": "input",
-        "messageKey": "CONFIG_REMINDER_2",
-        "defaultValue": "Drink Water",
-        "label": "Second Reminder",
-        "attributes": { "maxlength": 64 }
-      },
-      {
-        "type": "input",
-        "messageKey": "CONFIG_REMINDER_3",
-        "defaultValue": "Stay Focused",
-        "label": "Third Reminder",
-        "attributes": { "maxlength": 64 }
-      }
+      { "type": "heading", "defaultValue": "Your Reminders" },
+      { "type": "input", "messageKey": "CONFIG_REMINDER_1", "defaultValue": "Keep Smiling", "label": "First Reminder", "attributes": { "maxlength": 48 } },
+      { "type": "input", "messageKey": "CONFIG_REMINDER_2", "defaultValue": "Drink Water", "label": "Second Reminder", "attributes": { "maxlength": 48 } },
+      { "type": "input", "messageKey": "CONFIG_REMINDER_3", "defaultValue": "Stay Focused", "label": "Third Reminder", "attributes": { "maxlength": 48 } }
     ]
   },
   {
     "type": "section",
     "items": [
-      {
-        "type": "heading",
-        "defaultValue": "Design & Colors"
-      },
-      {
-        "type": "color",
-        "messageKey": "CONFIG_COLOR_TOP",
-        "defaultValue": "0x55AAFF",
-        "label": "Top Background Color",
-        "sunlight": true
-      },
-      {
-        "type": "toggle",
-        "messageKey": "CONFIG_OUTLINE_WHITE",
-        "label": "Use White Outline for Time",
-        "defaultValue": false
-      }
+      { "type": "heading", "defaultValue": "Top Area & Time" },
+      { "type": "color", "messageKey": "CONFIG_COLOR_TOP", "defaultValue": "0x55AAFF", "label": "Top Background Color" },
+      { "type": "color", "messageKey": "CONFIG_COLOR_TIME", "defaultValue": "0xFF0000", "label": "Time Color" },
+      { "type": "color", "messageKey": "CONFIG_COLOR_BATTERY", "defaultValue": "0xFF0000", "label": "Battery Bar Color" },
+      { "type": "toggle", "messageKey": "CONFIG_OUTLINE_WHITE", "label": "White Outline (Time)", "defaultValue": false, "description": "Enable this for better readability on dark top colors." },
+      { "type": "toggle", "messageKey": "CONFIG_HIDE_ICON", "label": "Hide Center Icon", "defaultValue": false, "description": "Hides the brain icon for a cleaner, minimalist look." }
     ]
   },
   {
-    "type": "submit",
-    "defaultValue": "Save Settings"
+    "type": "section",
+    "items": [
+      { "type": "heading", "defaultValue": "Reminder Field" },
+      { "type": "color", "messageKey": "CONFIG_COLOR_TEXT", "defaultValue": "0x000055", "label": "Reminder Text Color" },
+      { "type": "color", "messageKey": "CONFIG_COLOR_BOTTOM", "defaultValue": "0xFFFFAA", "label": "Background (Start Color)" },
+      { "type": "toggle", "messageKey": "CONFIG_ENABLE_GRADIENT", "label": "Enable Smooth Gradient", "defaultValue": false },
+      { "type": "color", "messageKey": "CONFIG_COLOR_BOTTOM_END", "defaultValue": "0x55AAFF", "label": "Gradient End Color" }
+    ]
+  },
+  { "type": "submit", "defaultValue": "Save Settings" },
+  {
+    "type": "text",
+    "defaultValue": "Made with love by <a href='https://atomlabor.de'>https://atomlabor.de</a>",
+    "attributes": { "style": "text-align: center; font-size: 12px; color: #888;" }
   }
 ];
